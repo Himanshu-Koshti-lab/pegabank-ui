a2512304c0283bdd03e5edce828bf120960d23f2
Himanshu Koshti <64060564+Himanshu-Koshti-lab@users.noreply.github.com>
2022-12-17 17:29:24 +0530

    Add Component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './common/about-us/about-us.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
+import { HomeComponent } from './common/home/home.component';
+import { ProfileComponent } from './common/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },{
+    path: 'Dashboard',
+    component: DashboardComponent
+  },{
+    path: 'Profile',
+    component: ProfileComponent
+  },{
+    path: 'About-us',
+    component: AboutUsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
